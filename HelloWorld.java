568dad950ff3451955d9dc08e4a8d8fdc24c84f1
Ustar7567 <85052449+Ustar7567@users.noreply.github.com>
2022-11-03 21:27:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
+      //CI practice
       System.out.println("Hello all, Shahataaz and Yogesh");
    }
 }
